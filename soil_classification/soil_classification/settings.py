670bc3495c7232e8fb1befa382a243a9a6b207ed
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-03-07 21:03:18 +0100

    Update settings.py

--- soil_classification/soil_classification/settings.py
+++ soil_classification/soil_classification/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i3iwa!0sm@rukw!4d#l4fb^vlipiv9a&zf4ra!zp!keggrc_r='
+SECRET_KEY = 'inset key here'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
