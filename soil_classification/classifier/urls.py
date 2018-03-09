677122ef0084c71c3c3ec1c9ff59186c5f94f1bf
Tim <35193581+tonzowonzo@users.noreply.github.com>
2018-03-09 18:17:02 +0100

    Update urls.py

--- soil_classification/classifier/urls.py
+++ soil_classification/classifier/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+import re
 
 urlpatterns = [
-        path('', views.title, name='title')
+        path('', views.title, name='title'),
+        path('time/', views.current_date_time, name='datetime'),
+        path('about/', views.about, name='about' ),
+        path('statistics/', views.statistics, name='statistics' ),
+        path('process/', views.process, name='process' ),
+        path('examples/', views.examples, name='examples' ),
+        path('contact/', views.contact, name='contact' ),
 ]
